80a0f772cad5e007751db9592144dca7604b6428
Jazzik42 <rmuzrrr@gmail.com>
2021-07-21 19:06:23 +0500

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,12 +4,32 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.utils.SqlRuDateTimeParser;
+
+import java.io.IOException;
+import java.util.stream.Stream;
 
 public class SqlRuParse {
+
+    public static Post init(String url) throws IOException {
+        Document page = Jsoup.connect(url).get();
+        Post post = null;
+        if (!(Stream.of("table[class=msgTable]", "td[class=messageHeader]",
+                "td[class=msgFooter]").allMatch(s -> page.select(s).isEmpty()))) {
+            Element page1 = page.select("table[class=msgTable]").first();
+            String titleText = page1.select("td[class=messageHeader]").first().text();
+            String date = page1.select("td[class=msgFooter]").first().text().split(" \\[")[0];
+            String descriptionText = page1.select("td[class=msgBody]").last().text();
+            SqlRuDateTimeParser dateD = new SqlRuDateTimeParser();
+            post = new Post(titleText, descriptionText, url, dateD.parse(date));
+        }
+        return post;
+    }
+
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        for (int i = 0; i < 5; i++) {
-            Elements listPage = doc.select(".sort_options");
+        Document doc;
+        for (int i = 1; i < 6; i++) {
+            doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + i).get();
             Elements row = doc.select(".postslisttopic");
             for (Element td : row) {
                 Element href = td.child(0);
@@ -18,10 +38,7 @@ public class SqlRuParse {
                 System.out.println(href.text());
                 System.out.println(el.text());
             }
-            Elements row2 = listPage.select("a");
-            Element hrefNextPage = row2.get(i);
             System.out.println("------------------------------------------------------");
-            doc = Jsoup.connect(hrefNextPage.attr("href")).get();
         }
     }
 }
