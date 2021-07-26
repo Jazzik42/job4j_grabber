904c34e4d06f9dd4dd6f86c786499f99b555a177
Jazzik42 <rmuzrrr@gmail.com>
2021-07-26 13:48:39 +0500

    2. SQL Select by company [#2760 #181517]

--- src/main/java/ru/job4j/grabber/utils/selectByCompany.sql
+++ src/main/java/ru/job4j/grabber/utils/selectByCompany.sql
@@ -13,5 +13,9 @@ CREATE TABLE person
     CONSTRAINT person_pkey PRIMARY KEY (id)
 );
 
-select pr.name, cn.name from person as pr join company as cn on pr.company_id=cn.id where cn.id != 5;
-select cn.name, count(ps.name) from company as cn join person as ps on ps.company_id=cn.id group by cn.name order by 2 Desc limit 1;
+select pr.name, cn.name from person as pr
+join company as cn on pr.company_id=cn.id where cn.id != 5;
+
+
+select cn.name, count(ps.name) from company as cn
+join person as ps on ps.company_id=cn.id group by cn.name order by count Desc limit 1;
